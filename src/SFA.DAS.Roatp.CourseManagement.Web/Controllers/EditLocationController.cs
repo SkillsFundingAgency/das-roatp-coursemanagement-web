9e4b7ac66846d1254faafc235c3859b7f93f717a
Deven Shah <devenshah1974@gmail.com>
2022-04-04 20:06:07 +0100

    CSP-34 Created web project

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/EditLocationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.Locations;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers
+{
+    public class EditLocationController : Controller
+    {
+        private readonly ILogger<EditLocationController> _logger;
+
+        public EditLocationController(ILogger<EditLocationController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new EditLocationViewModel { Name = "Coventry" });
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/Locations/EditLocationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.Web.Models.Locations
+{
+    public class EditLocationViewModel
+    {
+        public string Name { get; set; }
+        public string Postcode { get; set; }
+    }
+}
