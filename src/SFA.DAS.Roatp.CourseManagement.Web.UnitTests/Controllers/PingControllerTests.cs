8ac5aae5de1ed0f6dbd3ba52ed826a348a1278ec
Deven Shah <devenshah1974@gmail.com>
2022-04-07 10:27:39 +0100

    CSP-34 Added tests for ping controller

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/PingControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Web.Controllers;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers
+{
+    [TestFixture]
+    public class PingControllerTests
+    {
+        [Test]
+        public void Ping_returns_Pong()
+        {
+            var controller = new PingController();
+            
+            var expectedResponse = "Pong";
+
+            var result = controller.Index() as OkObjectResult;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Value, Is.EqualTo(expectedResponse));
+        }
+    }
+}
--- src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/EditLocationController.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/EditLocationController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.CourseManagement.Web.Models.Locations;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers
 {
+    [ExcludeFromCodeCoverage] // just a plcase holder at the moment, will be removed or repurposed 
     public class EditLocationController : Controller
     {
         private readonly ILogger<EditLocationController> _logger;
