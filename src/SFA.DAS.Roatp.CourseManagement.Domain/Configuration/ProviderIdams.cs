27fafc1461d7d25563593d7644fc85360677c89d
Deven Shah <devenshah1974@gmail.com>
2022-04-21 20:43:31 +0100

    CSP-31 Added domain project and configuration files

---
+++ src/SFA.DAS.Roatp.CourseManagement.Domain/Configuration/ProviderIdams.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.Domain.Configuration
+{
+    public class ProviderIdams
+    {
+        public string MetadataAddress { get; set; }
+
+        public string Wtrealm { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Domain/Configuration/RoatpCourseManagement.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.Domain.Configuration
+{
+    public class RoatpCourseManagement
+    {
+        public string RedisConnectionString { get; set; }
+        public string DataProtectionKeysDatabase { get; set; }
+    }
+}
