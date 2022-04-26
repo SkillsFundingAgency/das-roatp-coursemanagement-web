99acea40a71a94f58c2da4046b7dddbb67f5fdc9
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-04-26 17:51:43 +0100

    CSP-15 view standards screen changes

--- src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/StandardsController.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/StandardsController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Authorization;
-using SFA.DAS.Roatp.CourseManagement.Web.Models.Locations;
 using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers
@@ -29,29 +28,18 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers
 
             _logger.LogInformation("Logged into course management with ukprn {ukprn}", ukprn);
 
-            ukprn = "10063272";
            var result = await _roatpCourseManagementOuterApiClient.GetAllStandards(int.Parse(ukprn));
 
-            var list = new ViewStandardsListViewModel
+            var model = new ViewStandardsListViewModel
             {
                 Standards = new System.Collections.Generic.List<ViewStandardsViewModel>()
             };
+            if(result != null)
+            {
+                model.Standards = result.Standards;
+            }
 
-            //var standard1 = new ViewStandardsViewModel
-            //{
-            //    CourseName = "test1",
-            //    IsImported = true
-            //};
-            //var standard2 = new ViewStandardsViewModel
-            //{
-            //    CourseName = "test2",
-            //    IsImported = false
-            //};
-            //list.Standards.Add(standard1);
-            //list.Standards.Add(standard2);
-            list.Standards = result.Standards;
-
-            return View(list);
+            return View(model);
         }
     }
 }
