bbe8bed8aa8c041186667ee6f35d51bfa79d3233
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-04-27 14:41:27 +0100

    CSP-15-Serializable attribute is removed

--- src/SFA.DAS.Roatp.CourseManagement.Web/Infrastructure/Exceptions/ApiClientException.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Infrastructure/Exceptions/ApiClientException.cs
@@ -9,7 +9,6 @@ using System.Text;
 namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Exceptions
 {
     [ExcludeFromCodeCoverage]
-    [Serializable]
     public class ApiClientException : ApplicationException
     {
         public string HttpMethod { get; }
