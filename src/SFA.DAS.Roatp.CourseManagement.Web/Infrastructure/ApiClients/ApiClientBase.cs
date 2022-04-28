781191e1d33f52351bb38b466384152dde96ea2f
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-04-28 18:46:56 +0100

    CSP-15 taken refactored apiclientBase code

--- src/SFA.DAS.Roatp.CourseManagement.Web/Infrastructure/ApiClients/ApiClientBase.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Infrastructure/ApiClients/ApiClientBase.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Firewall;
+﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
 {
@@ -17,9 +18,9 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
     [ExcludeFromCodeCoverage]
     public abstract class ApiClientBase<AC>
     {
-        private const string _acceptHeaderName = "Accept";
+        protected const string _acceptHeaderName = "Accept";
         protected const string _contentType = "application/json";
-        
+
         protected readonly HttpClient _httpClient;
         protected readonly ILogger<AC> _logger;
 
@@ -41,14 +42,20 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
         /// <param name="uri">The URI to the end point you wish to interact with.</param>
         /// <returns>A Task yielding the result (of type T).</returns>
         /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<T> Get<T>(string uri) where T : new()
+        protected async Task<T> Get<T>(string uri)
         {
             try
             {
                 using (var response = await _httpClient.GetAsync(new Uri(uri, UriKind.Relative)))
                 {
+
                     await LogErrorIfUnsuccessfulResponse(response);
-                    return await response.Content.ReadAsAsync<T>();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await response.Content.ReadAsAsync<T>();
+                    }
+                    return default;
+
                 }
             }
             catch (HttpRequestException ex)
@@ -59,58 +66,48 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
         }
 
         /// <summary>
-        /// HTTP GET to the specified URI
+        /// HTTP POST to the specified URI
         /// </summary>
         /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>A Task yielding the result (as a string).</returns>
+        /// <returns>The HttpStatusCode, which is the responsibility of the caller to handle.</returns>
         /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<string> Get(string uri)
+        protected async Task<HttpStatusCode> Post<T>(string uri, T model)
         {
+            var serializeObject = JsonConvert.SerializeObject(model);
+
             try
             {
-                using (var response = await _httpClient.GetAsync(new Uri(uri, UriKind.Relative)))
+                using (var response = await _httpClient.PostAsync(new Uri(uri, UriKind.Relative),
+                    new StringContent(serializeObject, Encoding.UTF8, _contentType)))
                 {
-                    await LogErrorIfUnsuccessfulResponse(response); 
-                    return await response.Content.ReadAsAsync<string>();
+                    await LogErrorIfUnsuccessfulResponse(response);
+                    return response.StatusCode;
                 }
             }
             catch (HttpRequestException ex)
             {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Get} - {uri}");
+                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Post} - {uri}");
                 throw;
             }
         }
-
 
         private async Task LogErrorIfUnsuccessfulResponse(HttpResponseMessage response)
         {
             if (response?.RequestMessage != null && !response.IsSuccessStatusCode)
             {
+                var callingMethod = new System.Diagnostics.StackFrame(1).GetMethod().Name;
+
                 var httpMethod = response.RequestMessage.Method.ToString();
                 var statusCode = (int)response.StatusCode;
                 var reasonPhrase = response.ReasonPhrase;
                 var requestUri = response.RequestMessage.RequestUri;
 
                 var responseContent = await response.Content.ReadAsStringAsync();
-                var message = TryParseJson<ApiError>(responseContent, out var apiError) ? apiError?.Message : responseContent;
+                var apiErrorMessage = responseContent;
 
-                _logger.LogError($"HTTP {statusCode} {reasonPhrase} || {httpMethod}: {requestUri} || Message: {message}");
+                _logger.LogError($"Method: {callingMethod} || HTTP {statusCode} {reasonPhrase} || {httpMethod}: {requestUri} || Message: {apiErrorMessage}");
             }
         }
 
-        private static bool TryParseJson<T>(string json, out T result) where T : new()
-        {
-            try
-            {
-                result = JsonConvert.DeserializeObject<T>(json);
-                return true;
-            }
-            catch (JsonException)
-            {
-                // The JSON is a different type
-                result = default(T);
-                return false;
-            }
-        }
     }
 }
