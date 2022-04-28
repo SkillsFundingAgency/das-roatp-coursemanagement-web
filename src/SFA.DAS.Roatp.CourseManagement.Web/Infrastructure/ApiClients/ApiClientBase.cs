a817176157b390c954b0b66db0c553e403654607
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-04-28 10:16:05 +0100

    CSP-15 out of scope code is removed

--- src/SFA.DAS.Roatp.CourseManagement.Web/Infrastructure/ApiClients/ApiClientBase.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Infrastructure/ApiClients/ApiClientBase.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Firewall;
-using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Exceptions;
 using System.Diagnostics.CodeAnalysis;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
@@ -51,7 +48,6 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
                 using (var response = await _httpClient.GetAsync(new Uri(uri, UriKind.Relative)))
                 {
                     await LogErrorIfUnsuccessfulResponse(response);
-                    ThrowExceptionIfUnsuccessfulResponse(response);
                     return await response.Content.ReadAsAsync<T>();
                 }
             }
@@ -75,7 +71,6 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
                 using (var response = await _httpClient.GetAsync(new Uri(uri, UriKind.Relative)))
                 {
                     await LogErrorIfUnsuccessfulResponse(response); 
-                    ThrowExceptionIfUnsuccessfulResponse(response);
                     return await response.Content.ReadAsAsync<string>();
                 }
             }
@@ -86,163 +81,6 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
             }
         }
 
-        /// <summary>
-        /// HTTP GET to the specified URI
-        /// </summary>
-        /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>The HttpResponseMessage, which is the responsibility of the caller to handle.</returns>
-        /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<HttpResponseMessage> GetResponse(string uri)
-        {
-            try
-            {
-                var response = await _httpClient.GetAsync(new Uri(uri, UriKind.Relative));
-
-                await LogErrorIfUnsuccessfulResponse(response);
-                ThrowExceptionIfUnsuccessfulResponse(response);
-                return response;
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Get} - {uri}");
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// HTTP POST to the specified URI
-        /// </summary>
-        /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>The HttpStatusCode, which is the responsibility of the caller to handle.</returns>
-        /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<HttpStatusCode> Post<T>(string uri, T model)
-        {
-            var serializeObject = JsonConvert.SerializeObject(model);
-
-            try
-            {
-                using (var response = await _httpClient.PostAsync(new Uri(uri, UriKind.Relative),
-                    new StringContent(serializeObject, Encoding.UTF8, _contentType)))
-                {
-                    await LogErrorIfUnsuccessfulResponse(response);
-                    ThrowExceptionIfUnsuccessfulResponse(response);
-                    return response.StatusCode;
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Post} - {uri}");
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// HTTP POST to the specified URI
-        /// </summary>
-        /// <typeparam name="T">The type of the object to POST.</typeparam>
-        /// <typeparam name="U">The type of the object to read.</typeparam>
-        /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>A Task yielding the result (of type U).</returns>
-        /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<U> Post<T, U>(string uri, T model) where U : new()
-        {
-            var serializeObject = JsonConvert.SerializeObject(model);
-
-            try
-            {
-                using (var response = await _httpClient.PostAsync(new Uri(uri, UriKind.Relative),
-                    new StringContent(serializeObject, Encoding.UTF8, _contentType)))
-                {
-                    await LogErrorIfUnsuccessfulResponse(response);
-                    ThrowExceptionIfUnsuccessfulResponse(response);
-                    return await response.Content.ReadAsAsync<U>();
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Post} - {uri}");
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// HTTP PUT to the specified URI
-        /// </summary>
-        /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>The HttpStatusCode, which is the responsibility of the caller to handle.</returns>
-        /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<HttpStatusCode> Put<T>(string uri, T model)
-        {
-            var serializeObject = JsonConvert.SerializeObject(model);
-
-            try
-            {
-                using (var response = await _httpClient.PutAsync(new Uri(uri, UriKind.Relative),
-                    new StringContent(serializeObject, Encoding.UTF8, _contentType)))
-                {
-                    await LogErrorIfUnsuccessfulResponse(response);
-                    ThrowExceptionIfUnsuccessfulResponse(response);
-                    return response.StatusCode;
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Put} - {uri}");
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// HTTP PUT to the specified URI
-        /// </summary>
-        /// <typeparam name="T">The type of the object to PUT.</typeparam>
-        /// <typeparam name="U">The type of the object to read.</typeparam>
-        /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>A Task yielding the result (of type U).</returns>
-        /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<U> Put<T, U>(string uri, T model) where U : new()
-        {
-            var serializeObject = JsonConvert.SerializeObject(model);
-
-            try
-            {
-                using (var response = await _httpClient.PutAsync(new Uri(uri, UriKind.Relative),
-                    new StringContent(serializeObject, Encoding.UTF8, _contentType)))
-                {
-                    await LogErrorIfUnsuccessfulResponse(response);
-                    ThrowExceptionIfUnsuccessfulResponse(response);
-                    return await response.Content.ReadAsAsync<U>();
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Put} - {uri}");
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// HTTP DELETE to the specified URI
-        /// </summary>
-        /// <param name="uri">The URI to the end point you wish to interact with.</param>
-        /// <returns>The HttpResponseMessage, which is the responsibility of the caller to handle.</returns>
-        /// <exception cref="HttpRequestException">Thrown if something unexpected occurred when sending the request.</exception>
-        protected async Task<HttpResponseMessage> DeleteResponse(string uri)
-        {
-            try
-            {
-                var response = await _httpClient.DeleteAsync(new Uri(uri, UriKind.Relative));
-
-                await LogErrorIfUnsuccessfulResponse(response);
-                ThrowExceptionIfUnsuccessfulResponse(response);
-                return response;
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError(ex, $"Error when processing request: {HttpMethod.Delete} - {uri}");
-                throw;
-            }
-        }
 
         private async Task LogErrorIfUnsuccessfulResponse(HttpResponseMessage response)
         {
@@ -257,16 +95,6 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.ApiClients
                 var message = TryParseJson<ApiError>(responseContent, out var apiError) ? apiError?.Message : responseContent;
 
                 _logger.LogError($"HTTP {statusCode} {reasonPhrase} || {httpMethod}: {requestUri} || Message: {message}");
-            }
-        }
-
-        private void ThrowExceptionIfUnsuccessfulResponse(HttpResponseMessage response)
-        {
-            if (response?.RequestMessage != null && !response.IsSuccessStatusCode)
-            {
-                var httpMethod = response.RequestMessage.Method.ToString();
-                var requestUri = response.RequestMessage.RequestUri;
-                throw new ApiClientException(response, $"Error when processing response: {httpMethod} - {requestUri}");
             }
         }
 
