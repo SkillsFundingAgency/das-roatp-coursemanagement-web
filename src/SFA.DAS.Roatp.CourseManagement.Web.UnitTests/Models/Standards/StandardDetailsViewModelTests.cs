34f82cc6b2475bff7e233f5b22f21bca192fbeed
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-05-16 17:49:23 +0100

    CSP-20-View Regulation Information

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/Standards/StandardDetailsViewModelTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Models.Standards
+{
+    [TestFixture]
+    public class StandardDetailsViewModelTests
+    {
+        [Test]
+        public void ImplicitOperater_ConvertsFromStandardDetails()
+        {
+            var standardDetails = new StandardDetails();
+
+            StandardDetailsViewModel viewModel = standardDetails;
+
+            viewModel.Should().BeEquivalentTo(standardDetails);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/Standards/StandardViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Models.Standards
+{
+
+    [TestFixture]
+    public class StandardViewModelTests
+    {
+        [Test]
+        public void ImplicitOperater_ConvertsFromStandard()
+        {
+            var standard = new Standard();
+
+            StandardViewModel viewModel = standard;
+
+            viewModel.Should().BeEquivalentTo(viewModel);
+        }
+    }
+}
