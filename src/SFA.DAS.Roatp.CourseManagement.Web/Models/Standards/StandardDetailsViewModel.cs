bfaf20aee636a4609a01d43bc54fe793f4f07710
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-05-16 15:42:00 +0100

    CSP-18 update

--- src/SFA.DAS.Roatp.CourseManagement.Web/Models/Standards/StandardDetailsViewModel.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/Standards/StandardDetailsViewModel.cs
@@ -13,6 +13,10 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Models.Standards
         public string RegulatorName { get; set; }
         public string Version { get; set; }
         public bool IsStandardRegulated => !string.IsNullOrEmpty(RegulatorName);
+        public string StandardInfoUrl { get; set; }
+        public string ContactUsPhoneNumber { get; set; }
+        public string ContactUsEmail { get; set; }
+        public string ContactUsPageUrl { get; set; }
         public string BackUrl { get; set; }
 
         public static implicit operator StandardDetailsViewModel(StandardDetails standardDetails)
@@ -25,7 +29,11 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Models.Standards
                 LarsCode = standardDetails.LarsCode,
                 RegulatorName = standardDetails.RegulatorName,
                 Sector = standardDetails.Sector,
-                Version = standardDetails.Version
+                Version = standardDetails.Version,
+                StandardInfoUrl = standardDetails.StandardInfoUrl,
+                ContactUsPhoneNumber = standardDetails.ContactUsPhoneNumber,
+                ContactUsEmail = standardDetails.ContactUsEmail,
+                ContactUsPageUrl = standardDetails.ContactUsPageUrl,
             };
         }
     }
