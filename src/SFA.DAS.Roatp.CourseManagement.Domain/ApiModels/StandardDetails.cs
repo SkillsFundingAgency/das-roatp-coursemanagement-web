9630d9f3f3915c7c216fa0e8a051604f5d3cae81
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2022-05-19 14:16:33 +0100

    CSP-18-web-Standard-ViewContactDetails (#14)
    * CSP-18-wip ViewContactDetails
    
    * CSP-18 code smell fix
    
    * CSP-18 update
    
    * CSP-18 h2 tags updated

--- src/SFA.DAS.Roatp.CourseManagement.Domain/ApiModels/StandardDetails.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Domain/ApiModels/StandardDetails.cs
@@ -9,5 +9,10 @@
         public int LarsCode { get; set; }
         public string RegulatorName { get; set; }
         public string Version { get; set; }
+        public string StandardInfoUrl { get; set; }
+        public string ContactUsPhoneNumber { get; set; }
+        public string ContactUsEmail { get; set; }
+        public string ContactUsPageUrl { get; set; }
+        public string BackUrl { get; set; }
     }
 }
