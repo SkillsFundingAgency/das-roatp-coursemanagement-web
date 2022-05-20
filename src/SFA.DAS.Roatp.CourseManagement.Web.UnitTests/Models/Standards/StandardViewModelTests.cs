f598f5aab89a12e5c2e74ef57a9a503f17dfbcc0
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2022-05-20 11:59:03 +0100

    CSP-20-ViewRegulationInformation (#15)
    * CSP-18-wip ViewContactDetails
    
    * CSP-18 code smell fix
    
    * CSP-18 update
    
    * CSP-20-View Regulation Information
    
    * CSP-20 hyper link in new tab
    
    * CSP-20 extended unit test
    
    * CSP-20 failing tests fixed
    
    * CSP-20 headers tags changed
    
    * Revert "CSP-20 failing tests fixed"
    
    This reverts commit eabf6595759f20f2810828ecd24aaea3f9af8cae.

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/Standards/StandardViewModelTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Models.Standards
+{
+
+    [TestFixture]
+    public class StandardViewModelTests
+    {
+        [Test]
+        public void ImplicitOperater_ConvertsFromStandard()
+        {
+            const int providerCourseId = 1;
+            const string courseName = "course name";
+            const int level = 1;
+            const int larsCode = 133;
+
+            var standard = new Standard
+            {
+                ProviderCourseId = providerCourseId,
+                CourseName = courseName,
+                LarsCode = larsCode,
+                Level = level,
+            };
+
+            StandardViewModel viewModel = standard;
+
+            viewModel.ProviderCourseId.Should().Be(providerCourseId);
+            viewModel.CourseName.Should().Be(courseName);
+            viewModel.Level.Should().Be(level);
+            viewModel.LarsCode.Should().Be(larsCode);
+            viewModel.Should().BeEquivalentTo(viewModel);
+
+        }
+    }
+}
