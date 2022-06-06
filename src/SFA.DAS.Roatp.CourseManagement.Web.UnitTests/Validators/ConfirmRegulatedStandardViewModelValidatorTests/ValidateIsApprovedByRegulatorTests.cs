39980477dd9abce5fe80c4cc694b056fa4838557
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-06-06 12:21:27 +0100

    CSP-21 unit test for Validator

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Validators/ConfirmRegulatedStandardViewModelValidatorTests/ValidateIsApprovedByRegulatorTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
+using SFA.DAS.Roatp.CourseManagement.Web.Validators;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Validators.ConfirmRegulatedStandardViewModelValidatorTests
+{
+    [TestFixture]
+    public class ValidateIsApprovedByRegulatorTests
+    {
+        [Test]
+        public void WhenNull_ProducesValidatonError()
+        {
+            var sut = new ConfirmRegulatedStandardViewModelValidator();
+
+            var command = new ConfirmRegulatedStandardViewModel();
+
+            var result = sut.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.IsApprovedByRegulator).WithErrorMessage("Select Yes or No");
+        }
+    }
+}
