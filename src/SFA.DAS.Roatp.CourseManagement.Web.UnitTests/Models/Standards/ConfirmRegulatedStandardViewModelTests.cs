5a2dfb16c92fa2fb72fd8ec8007839b9165e3e4d
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-06-14 19:01:42 +0100

    CSP-105 review comment

--- src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/Standards/ConfirmRegulatedStandardViewModelTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/Standards/ConfirmRegulatedStandardViewModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Application.Standard.Commands.UpdateApprovedByRegulator;
 using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
 using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
 
@@ -17,6 +18,19 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Models.Standards
             vm.Should().BeEquivalentTo(standard, o =>
             {
                 o.Including(c => c.RegulatorName);
+                o.Including(c => c.IsApprovedByRegulator);
+                return o;
+            });
+        }
+
+        [Test, AutoData]
+        public void ImplicitOperatorForCommand_ReturnsCommand(ConfirmRegulatedStandardViewModel model)
+        {
+            var vm = (UpdateApprovedByRegulatorCommand)model;
+
+            vm.Should().BeEquivalentTo(model, o =>
+            {
+                o.Including(c => c.LarsCode);
                 o.Including(c => c.IsApprovedByRegulator);
                 return o;
             });
