59d7ad2ad2ed08fc0035bd715518a5508124e971
Deven Shah <devenshah1974@gmail.com>
2022-06-14 08:33:24 +0100

    CSP-81 Excluding couple of files from code coverage

--- src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/ControllerBase.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/ControllerBase.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Authorization;
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers
 {
+    [ExcludeFromCodeCoverage]
     public abstract class ControllerBase : Controller
     {
         protected int Ukprn => int.Parse(User.FindFirstValue(ProviderClaims.ProviderUkprn));
