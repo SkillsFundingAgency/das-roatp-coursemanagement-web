98272f0a8bc5952b36d253ed3003b81263997009
Deven Shah <devenshah1974@gmail.com>
2022-06-21 01:01:18 +0100

    CSP-81 Added  session service

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/AppStart/AddServiceRegistraionsExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Roatp.CourseManagement.Domain.Configuration;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
+using SFA.DAS.Roatp.CourseManagement.Infrastructure.ApiClients;
+using SFA.DAS.Roatp.CourseManagement.Web.Services;
+using System;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.AppStart
+{
+    public static class AddServiceRegistraionsExtension
+    {
+        public static void AddServiceRegistrations(this IServiceCollection services, IConfiguration configuration)
+        {
+            ConfigureHttpClient(services, configuration);
+            services.AddHttpContextAccessor();
+            services.AddTransient<ISessionService, SessionService>();
+        }
+        private static void ConfigureHttpClient(IServiceCollection services, IConfiguration configuration)
+        {
+            var handlerLifeTime = TimeSpan.FromMinutes(5);
+            services.AddHttpClient<IApiClient, ApiClient>(config =>
+            {
+                var outerApiConfiguration = configuration
+                    .GetSection(nameof(RoatpCourseManagementOuterApi))
+                    .Get<RoatpCourseManagementOuterApi>();
+
+                config.BaseAddress = new Uri(outerApiConfiguration.BaseUrl);
+                config.DefaultRequestHeaders.Add("Accept", "application/json");
+                config.DefaultRequestHeaders.Add("X-Version", "1");
+                config.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", outerApiConfiguration.SubscriptionKey);
+            })
+           .SetHandlerLifetime(handlerLifeTime);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Services/SessionService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Services
+{
+    public class SessionService : ISessionService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SessionService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Set(string key, string value) =>
+            _httpContextAccessor.HttpContext.Session.SetString(key, value);
+
+        public string Get(string key) =>
+            _httpContextAccessor.HttpContext.Session.GetString(key);
+
+        public void Delete(string key) =>
+            _httpContextAccessor.HttpContext.Session.Remove(key);
+
+        public void Set<T>(T model) =>
+            Set(typeof(T).Name, JsonSerializer.Serialize(model));
+
+        public T Get<T>()
+        {
+            var json = Get(typeof(T).Name);
+            if (string.IsNullOrEmpty(json)) return default;
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
