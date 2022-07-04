84f1184da980f524421c4e3c75ccff94cb2b2256
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-07-04 21:41:04 +0100

    CSP-114 updated tests after code refactor

--- src/SFA.DAS.Roatp.CourseManagement.Application.UnitTests/Handlers/GetAllStandardRegionsQueryHandlerTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Application.UnitTests/Handlers/GetAllStandardRegionsQueryHandlerTests.cs
@@ -19,7 +19,7 @@ namespace SFA.DAS.Roatp.CourseManagement.Application.UnitTests.Handlers
         private GetAllStandardRegionsQueryHandler _handler;
         private Mock<IApiClient> _apiClient;
         private GetAllStandardRegionsQuery _query;
-        private readonly Region _regionalLocation = new Region() { Id =1, RegionName="Test", SubregionName="Test" };
+        private GetAllStandardRegionsQueryResult _result;
 
         [SetUp]
         public void Setup()
@@ -27,6 +27,7 @@ namespace SFA.DAS.Roatp.CourseManagement.Application.UnitTests.Handlers
             var autoFixture = new Fixture();
 
             _query = autoFixture.Create<GetAllStandardRegionsQuery>();
+            _result = autoFixture.Create<GetAllStandardRegionsQueryResult>();
             _apiClient = new Mock<IApiClient>();
             _handler = new GetAllStandardRegionsQueryHandler(_apiClient.Object, Mock.Of<ILogger<GetAllStandardRegionsQueryHandler>>());
         }
@@ -34,46 +35,17 @@ namespace SFA.DAS.Roatp.CourseManagement.Application.UnitTests.Handlers
         [Test]
         public async Task Handle_ValidApiRequest_ReturnsValidResponseNoSelectRegions()
         {
-            var regions = new List<Region> { _regionalLocation };
-            _apiClient.Setup(x => x.Get<List<Region>>($"lookup/regions")).ReturnsAsync(regions);
-            _apiClient.Setup(x => x.Get<StandardDetails>($"providers/{_query.Ukprn}/courses/{_query.LarsCode}")).ReturnsAsync(new StandardDetails());
+            _apiClient.Setup(x => x.Get<GetAllStandardRegionsQueryResult>($"providers/{_query.Ukprn}/courses/{_query.LarsCode}/standardsubregions")).ReturnsAsync(_result);
 
             var result = await _handler.Handle(_query, CancellationToken.None);
 
             result.Should().NotBeNull();
-            result.Regions.Should().BeEquivalentTo(regions);
-            result.Regions.TrueForAll(a => a.IsSelected).Should().BeFalse();
         }
 
         [Test]
-        public async Task Handle_ValidApiRequest_ReturnsValidResponseWithSelectedRegions()
+        public void Handle_NoRegionsData_ThrowsException()
         {
-            var regions = new List<Region> { _regionalLocation };
-            _apiClient.Setup(x => x.Get<List<Region>>($"lookup/regions")).ReturnsAsync(regions);
-            var standardDetails = new StandardDetails { ProviderCourseLocations = new List<ProviderCourseLocation> { new ProviderCourseLocation { LocationName = regions[0].RegionName, LocationType = LocationType.Regional } } };
-            _apiClient.Setup(x => x.Get<StandardDetails>($"providers/{_query.Ukprn}/courses/{_query.LarsCode}")).ReturnsAsync(standardDetails);
-
-            var result = await _handler.Handle(_query, CancellationToken.None);
-
-            result.Should().NotBeNull();
-            result.Regions.Should().BeEquivalentTo(regions);
-            result.Regions.FindAll(a => a.IsSelected).Count.Should().Be(1);
-        }
-
-        [Test]
-        public void Handle_InvalidApiResponse_ThrowsException()
-        {
-            _apiClient.Setup(x => x.Get<List<Region>>($"lookup/regions")).ReturnsAsync((List<Region>)null);
-
-            Assert.ThrowsAsync<ValidationException>(() => _handler.Handle(_query, CancellationToken.None));
-        }
-
-        [Test]
-        public void Handle_NoProviderCourses_ThrowsException()
-        {
-            var regions = new List<Region> { _regionalLocation };
-            _apiClient.Setup(x => x.Get<List<Region>>($"lookup/regions")).ReturnsAsync(regions);
-            _apiClient.Setup(x => x.Get<StandardDetails>($"providers/{_query.Ukprn}/courses/{_query.LarsCode}")).ReturnsAsync((StandardDetails)null);
+            _apiClient.Setup(x => x.Get<GetAllStandardRegionsQueryResult>($"providers/{_query.Ukprn}/courses/{_query.LarsCode}/standardsubregions")).ReturnsAsync((GetAllStandardRegionsQueryResult)null);
 
             Assert.ThrowsAsync<ValidationException>(() => _handler.Handle(_query, CancellationToken.None));
         }
