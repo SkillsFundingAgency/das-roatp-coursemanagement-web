df0e0d2472d01d33c6dbe345b5e427489880ecbf
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-07-04 18:02:18 +0100

    CSP-114 review comment

--- src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAllStandardRegions/GetAllStandardRegionsQueryHandler.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAllStandardRegions/GetAllStandardRegionsQueryHandler.cs
@@ -22,35 +22,18 @@ namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.G
         }
         public async Task<GetAllStandardRegionsQueryResult> Handle(GetAllStandardRegionsQuery request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Get All Regions request received");
-            var regions = await _apiClient.Get<List<Region>>($"lookup/regions");
-            if (regions == null)
+            _logger.LogInformation("Get All Standard Regions request received for ukprn {ukprn} and larsCode {larsCode}", request.Ukprn, request.LarsCode);
+
+            var allStandardRegions = await _apiClient.Get<GetAllStandardRegionsQueryResult>($"providers/{request.Ukprn}/courses/{request.LarsCode}/standardsubregions");
+            if (allStandardRegions == null)
             {
-                var message = "All Regions not found";
+                var message = $"All Standard Regions not found for ukprn {request.Ukprn} and larsCode {request.LarsCode}";
                 _logger.LogError(message);
                 throw new ValidationException(message);
             }
-
-            _logger.LogInformation("Get Standards details request received for ukprn {ukprn} and larsCode {larsCode}", request.Ukprn, request.LarsCode);
-            var standardDetails = await _apiClient.Get<StandardDetails>($"providers/{request.Ukprn}/courses/{request.LarsCode}");
-            if (standardDetails == null)
-            {
-                var message = $"Standard details not found for ukprn {request.Ukprn} and LarsCode {request.LarsCode}";
-                _logger.LogError(message);
-                throw new ValidationException(message);
-            }
-            var subRegionCourseLocations = standardDetails.ProviderCourseLocations.Where(a => a.LocationType == LocationType.Regional).ToList();
-            if(subRegionCourseLocations.Any())
-            {
-                foreach (var region in regions)
-                {
-                    region.IsSelected = subRegionCourseLocations.Exists(r => r.LocationName == region.SubregionName);
-                }
-            }
-
             return new GetAllStandardRegionsQueryResult
             {
-                Regions = regions
+                Regions = allStandardRegions.Regions
             };
         }
     }
