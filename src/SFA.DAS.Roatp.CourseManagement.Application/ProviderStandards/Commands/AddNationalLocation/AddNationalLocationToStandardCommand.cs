11eadc9d3353a12e6649c73e472a4a1824c68d24
Deven Shah <devenshah1974@gmail.com>
2022-07-08 19:00:42 +0100

    CSP-122 Added handler to add national location to the course

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Commands/AddNationalLocation/AddNationalLocationToStandardCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Commands.AddNationalLocation
+{
+    public class AddNationalLocationToStandardCommand : IRequest<Unit>
+    {
+        public int Ukprn { get; }
+        public int LarsCode { get; }
+        public string UserId { get; }
+        public AddNationalLocationToStandardCommand(int ukprn, int larsCode, string userId)
+        {
+            Ukprn = ukprn;
+            LarsCode = larsCode;
+            UserId = userId;
+        }
+    }
+}
