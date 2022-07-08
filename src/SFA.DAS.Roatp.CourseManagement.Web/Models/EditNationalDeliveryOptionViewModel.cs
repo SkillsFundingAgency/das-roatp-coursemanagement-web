fecc75317c000cef868fe68d3992c7fa59f8517a
Deven Shah <devenshah1974@gmail.com>
2022-07-08 23:14:13 +0100

    CSP-112 Added national delivery model and validator

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/EditNationalDeliveryOptionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.Web.Models
+{
+    public class EditNationalDeliveryOptionViewModel
+    {
+        public bool? HasNationalDeliveryOption { get; set; }
+        public string BackLink { get; set; } = "#";
+        public string CancelLink { get; set; } = "#";
+    }
+}
