dd02999d464a6a9524de9b9a3d57495eb5cc1016
Deven Shah <devenshah1974@gmail.com>
2022-07-14 12:45:50 +0100

    CSP-13 Added model and validator for postcode input

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Validators/ProviderLocationPostcodeViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.ProviderLocations.AddProviderLocation;
+using System.Text.RegularExpressions;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Validators
+{
+    public class ProviderLocationPostcodeViewModelValidator : AbstractValidator<ProviderLocationPostcodeViewModel>
+    {
+        public const string PostcodeEmptyMessage = "You must enter a postcode";
+        public const string PostcodeInvalidMessage = "Enter a real postcode";
+        public const string PostcodeRegex = @"^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$";
+        public ProviderLocationPostcodeViewModelValidator()
+        {
+            RuleFor(m => m.Postcode)
+                .NotEmpty()
+                .WithMessage(PostcodeEmptyMessage)
+                .Matches(PostcodeRegex, RegexOptions.IgnoreCase)
+                .WithMessage(PostcodeInvalidMessage);
+        }
+    }
+}
