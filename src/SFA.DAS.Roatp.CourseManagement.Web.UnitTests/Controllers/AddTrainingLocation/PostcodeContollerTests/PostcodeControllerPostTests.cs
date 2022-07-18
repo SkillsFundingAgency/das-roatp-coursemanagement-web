44770f64349fa672e221c040a7608ab168fe2c1b
Deven Shah <devenshah1974@gmail.com>
2022-07-18 23:55:00 +0100

    CSP-13 Refactored to organise the journey

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/AddTrainingLocation/PostcodeContollerTests/PostcodeControllerPostTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Web.Controllers.AddTrainingLocation;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation;
+using SFA.DAS.Roatp.CourseManagement.Web.UnitTests.TestHelpers;
+using System;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.AddTrainingLocation.PostcodeContollerTests
+{
+    [TestFixture]
+    public class PostcodeControllerPostTests
+    {
+        PostcodeController Sut;
+        readonly string _providerLocationUrl = Guid.NewGuid().ToString();
+
+        [SetUp]
+        public void Before_Each_Tests()
+        {
+            Sut = new PostcodeController();
+            Sut
+                .AddDefaultContextWithUser()
+                .AddUrlHelperMock()
+                .AddUrlForRoute(RouteNames.GetProviderLocations, _providerLocationUrl);
+        }
+        [Test]
+        public void Post_ModelStateIsInvalid_ReturnsViewResult()
+        {
+            Sut.ModelState.AddModelError("errorKey", "errorMessage");
+
+            var result = (ViewResult)Sut.SubmitPostcode(new PostcodeViewModel());
+
+            result.Should().NotBeNull();
+            var model = (PostcodeViewModel)result.Model;
+            model.BackLink.Should().Be(_providerLocationUrl);
+            model.CancelLink.Should().Be(_providerLocationUrl);
+        }
+
+        [Test, AutoData]
+        public void Post_ModelStateIsValid_RedirectsToGetTrainingLocationAddress(PostcodeSubmitModel model)
+        {
+            var result = (RedirectToRouteResult)Sut.SubmitPostcode(model);
+
+            result.RouteName.Should().Be(RouteNames.GetTrainingLocationAddress);
+            result.RouteValues.Should().HaveCount(2);
+            result.RouteValues[nameof(model.Postcode)].Should().Be(model.Postcode);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/AddTrainingLocation/PostcodeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Authorization.Mvc.Attributes;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Authorization;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers.AddTrainingLocation
+{
+    [DasAuthorize(new[] { "ProviderFeature.CourseManagement" }, Policy = nameof(PolicyNames.HasProviderAccount))]
+    public class PostcodeController : ControllerBase
+    {
+        public const string ViewPath = "~/Views/AddTrainingLocation/Postcode.cshtml";
+        [Route("{ukprn}/add-training-location/postcode", Name = RouteNames.GetTrainingLocationPostcode)]
+        [HttpGet]
+        public IActionResult GetPostcode()
+        {
+            var model = new PostcodeViewModel();
+            model.BackLink = model.CancelLink = Url.RouteUrl(RouteNames.GetProviderLocations, new { Ukprn });
+            return View(ViewPath, model);
+        }
+
+        [Route("{ukprn}/add-training-location/postcode", Name = RouteNames.PostTrainingLocationPostcode)]
+        [HttpPost]
+        public IActionResult SubmitPostcode(PostcodeSubmitModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return GetPostcode();
+            }
+
+            return RedirectToRoute(RouteNames.GetTrainingLocationAddress, new { Ukprn, model.Postcode });
+        }
+    }
+}
