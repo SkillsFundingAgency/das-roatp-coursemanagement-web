2400ee52bd2ec6dc0f2bc2e1387ebf233a920324
Deven Shah <devenshah1974@gmail.com>
2022-07-19 00:57:40 +0100

    CSP-48 Persist postcode in session

--- src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/AddTrainingLocation/PostcodeContollerTests/PostcodeControllerGetTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/AddTrainingLocation/PostcodeContollerTests/PostcodeControllerGetTests.cs
@@ -15,23 +15,33 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.AddTrainingLo
     [TestFixture]
     public class PostcodeControllerGetTests
     {
-        PostcodeController Sut;
-        readonly string _providerLocationUrl = Guid.NewGuid().ToString();
+        private readonly string _providerLocationUrl = Guid.NewGuid().ToString();
+        private Mock<ISessionService> _sessionServiceMock;
+        private PostcodeController _sut;
 
         [SetUp]
         public void Before_Each_Tests()
         {
-            Sut = new PostcodeController();
-            Sut
+            _sessionServiceMock = new Mock<ISessionService>();
+            _sut = new PostcodeController(_sessionServiceMock.Object);
+            _sut
                 .AddDefaultContextWithUser()
                 .AddUrlHelperMock()
                 .AddUrlForRoute(RouteNames.GetProviderLocations, _providerLocationUrl);
         }
 
         [Test]
+        public void Get_DeletesPostcodeFromSession()
+        {
+            _sut.GetPostcode();
+
+            _sessionServiceMock.Verify(s => s.Delete(SessionKeys.SelectedPostcode, TestConstants.DefaultUkprn));
+        }
+
+        [Test]
         public void Get_ReturnsViewResult()
         {
-            var result = (ViewResult)Sut.GetPostcode();
+            var result = (ViewResult)_sut.GetPostcode();
 
             result.Should().NotBeNull();
             var model = (PostcodeViewModel)result.Model;
