79f138c18982d55febe88d3f7d729c146005543f
Deven Shah <devenshah1974@gmail.com>
2022-07-19 18:39:37 +0100

    CSP-48 Added query handler for addresses

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderLocations/Queries/GetAddresses/GetAddressesQueryResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Queries.GetAddresses
+{
+    public class GetAddressesQueryResult
+    {
+        public List<AddressItem> Addresses { get; set; }
+    }
+}
