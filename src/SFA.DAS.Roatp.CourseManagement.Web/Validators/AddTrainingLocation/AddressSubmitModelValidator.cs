3811fce176bf6423081dc31c53e6a21117465cc2
Deven Shah <devenshah1974@gmail.com>
2022-07-19 18:03:27 +0100

    CSP-48 Added model and its validator

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Validators/AddTrainingLocation/AddressSubmitModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Validators.AddTrainingLocation
+{
+    public class AddressSubmitModelValidator : AbstractValidator<AddressSubmitModel>
+    {
+        public const string AddressNotSelectedErrorMessage = "You must choose an address from the list";
+        public AddressSubmitModelValidator()
+        {
+            RuleFor(m => m.SelectedAddressUprn)
+                .NotEmpty()
+                .WithMessage(AddressNotSelectedErrorMessage);
+        }
+    }
+}
