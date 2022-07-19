670a1b79ab65763c5ef879f9873e19a0e4409ae4
Deven Shah <devenshah1974@gmail.com>
2022-07-19 17:55:15 +0100

    CSP-48 Renamed postcode validator to match model name

--- src/SFA.DAS.Roatp.CourseManagement.Web/Validators/AddTrainingLocation/PostcodeSubmitModelValidator.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Validators/AddTrainingLocation/PostcodeSubmitModelValidator.cs
@@ -4,12 +4,12 @@ using System.Text.RegularExpressions;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Validators.AddTrainingLocation
 {
-    public class PostcodePostModelValidator : AbstractValidator<PostcodeSubmitModel>
+    public class PostcodeSubmitModelValidator : AbstractValidator<PostcodeSubmitModel>
     {
         public const string PostcodeEmptyMessage = "You must enter a postcode";
         public const string PostcodeInvalidMessage = "Enter a real postcode";
         public const string PostcodeRegex = @"^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$";
-        public PostcodePostModelValidator()
+        public PostcodeSubmitModelValidator()
         {
             RuleFor(m => m.Postcode)
                 .NotEmpty()
