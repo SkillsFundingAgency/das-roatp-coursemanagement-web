4fc84e28cfdd8eac010f780f7ffb28b62d91766a
Deven Shah <devenshah1974@gmail.com>
2022-07-19 18:39:37 +0100

    CSP-48 Added query handler for addresses

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderLocations/Queries/GetAddresses/GetAddressesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Queries.GetAddresses
+{
+    public class GetAddressesQueryHandler : IRequestHandler<GetAddressesQuery, GetAddressesQueryResult>
+    {
+        private readonly IApiClient _apiClient;
+
+        public GetAddressesQueryHandler(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<GetAddressesQueryResult> Handle(GetAddressesQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _apiClient.Get<GetAddressesQueryResult>($"lookup/addresses?postcode={HttpUtility.UrlEncode(request.Postcode)}");
+            return response;
+        }
+    }
+}
