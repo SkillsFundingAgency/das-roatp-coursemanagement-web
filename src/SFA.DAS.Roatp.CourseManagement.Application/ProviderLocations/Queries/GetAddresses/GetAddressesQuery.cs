93918ec8f57844231050a829a66d823e60473d13
Deven Shah <devenshah1974@gmail.com>
2022-07-19 18:39:37 +0100

    CSP-48 Added query handler for addresses

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderLocations/Queries/GetAddresses/GetAddressesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Queries.GetAddresses
+{
+    public class GetAddressesQuery : IRequest<GetAddressesQueryResult>
+    {
+        public string Postcode { get; }
+        public GetAddressesQuery(string postcode)
+        {
+            Postcode = postcode;
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Domain/ApiModels/AddressItem.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.Domain.ApiModels
+{
+    public class AddressItem
+    {
+        public string Uprn { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Town { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
