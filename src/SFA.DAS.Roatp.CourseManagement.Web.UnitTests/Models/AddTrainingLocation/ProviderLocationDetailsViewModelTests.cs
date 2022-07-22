7700ef02da38f446725beda07d002089ba8fc735
Deven Shah <devenshah1974@gmail.com>
2022-07-22 17:13:47 +0100

    CSP-48 Implements view to capture location details

--- src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/AddTrainingLocation/ProviderLocationDetailsViewModelTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/AddTrainingLocation/ProviderLocationDetailsViewModelTests.cs
@@ -7,12 +7,12 @@ using SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation;
 namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Models.AddTrainingLocation
 {
     [TestFixture]
-    public class TrainingLocationDetailsViewModelTests
+    public class ProviderLocationDetailsViewModelTests
     {
         [Test, AutoData]
         public void Constructor_BuildsModel(AddressItem addressItem)
         {
-            var sut = new TrainingLocationDetailsViewModel(addressItem);
+            var sut = new ProviderLocationDetailsViewModel(addressItem);
             sut.AddressLine1.Should().Be(addressItem.AddressLine1);
             sut.AddressLine2.Should().Be(addressItem.AddressLine2);
             sut.Town.Should().Be(addressItem.Town);
