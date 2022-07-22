e04ad210371a41db672cf279c42f5e6140613c36
Deven Shah <devenshah1974@gmail.com>
2022-07-22 14:17:25 +0100

    CSP-48 added view model for location details

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddTrainingLocation/TrainingLocationDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation
+{
+    public class TrainingLocationDetailsViewModel : TrainingLocationDetailsSubmitModel
+    {
+        public TrainingLocationDetailsViewModel(AddressItem addressItem)
+        {
+            AddressLine1 = addressItem.AddressLine1;
+            AddressLine2 = addressItem.AddressLine2;
+            Town = addressItem.Town;
+            Postcode = addressItem.Postcode;
+        }
+
+        public string BackLink { get; set; } = "#";
+        public string CancelLink { get; set; } = "#";
+    }
+
+    public class TrainingLocationDetailsSubmitModel
+    {
+        public string LocationName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+        public string Website { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
