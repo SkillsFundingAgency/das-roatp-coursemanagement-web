99ac4f3be5fceb128eb31873aeb26458647ab80e
Deven Shah <devenshah1974@gmail.com>
2022-07-22 14:17:25 +0100

    CSP-48 added view model for location details

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Models/AddTrainingLocation/TrainingLocationDetailsViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Models.AddTrainingLocation
+{
+    [TestFixture]
+    public class TrainingLocationDetailsViewModelTests
+    {
+        [Test, AutoData]
+        public void Constructor_BuildsModel(AddressItem addressItem)
+        {
+            var sut = new TrainingLocationDetailsViewModel(addressItem);
+            sut.AddressLine1.Should().Be(addressItem.AddressLine1);
+            sut.AddressLine2.Should().Be(addressItem.AddressLine2);
+            sut.Town.Should().Be(addressItem.Town);
+            sut.Postcode.Should().Be(addressItem.Postcode);
+        }
+    }
+}
