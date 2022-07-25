3824d9c5faea4a2bb285c6e4b018e140a78fd48a
Deven Shah <devenshah1974@gmail.com>
2022-07-25 15:18:36 +0100

    CSP-14 Fixed lat and long to be of decimal type

--- src/SFA.DAS.Roatp.CourseManagement.Domain/ApiModels/AddressItem.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Domain/ApiModels/AddressItem.cs
@@ -8,7 +8,7 @@
         public string Town { get; set; }
         public string County { get; set; }
         public string Postcode { get; set; }
-        public double? Latitude { get; set; }
-        public double? Longitude { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
     }
 }
