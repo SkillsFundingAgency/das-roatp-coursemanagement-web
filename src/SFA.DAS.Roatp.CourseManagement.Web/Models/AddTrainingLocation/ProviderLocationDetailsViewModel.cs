267995082bfc2a7bdd23168267aa508276b27c3c
Deven Shah <devenshah1974@gmail.com>
2022-07-25 16:43:53 +0100

    CSP-14 Added submit model validation

--- src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddTrainingLocation/ProviderLocationDetailsViewModel.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddTrainingLocation/ProviderLocationDetailsViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation
 {
-    public class ProviderLocationDetailsViewModel : TrainingLocationDetailsSubmitModel
+    public class ProviderLocationDetailsViewModel : ProviderLocationDetailsSubmitModel
     {
         public ProviderLocationDetailsViewModel(AddressItem addressItem)
         {
@@ -14,15 +14,15 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation
 
         public string BackLink { get; set; } = "#";
         public string CancelLink { get; set; } = "#";
-    }
-
-    public class TrainingLocationDetailsSubmitModel
-    {
-        public string LocationName { get; set; }
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
         public string Town { get; set; }
         public string Postcode { get; set; }
+    }
+
+    public class ProviderLocationDetailsSubmitModel
+    {
+        public string LocationName { get; set; }
         public string Website { get; set; }
         public string EmailAddress { get; set; }
         public string PhoneNumber { get; set; }
