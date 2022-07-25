5a89975a0849854897678fa6d29ced468f948116
Deven Shah <devenshah1974@gmail.com>
2022-07-25 17:53:46 +0100

    CSP-14 Added controller post action for creating provider location

--- src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/AddTrainingLocation/AddProviderLocationDetailsController.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/AddTrainingLocation/AddProviderLocationDetailsController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Authorization.Mvc.Attributes;
+using SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Commands.CreateProviderLocation;
 using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Authorization;
 using SFA.DAS.Roatp.CourseManagement.Web.Models.AddTrainingLocation;
+using System;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -15,45 +18,93 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers.AddTrainingLocation
     {
         public const string ViewPath = "~/Views/AddTrainingLocation/AddTrainingLocationDetails.cshtml";
         private readonly ILogger<AddProviderLocationDetailsController> _logger;
+        private readonly IMediator _mediator;
 
-        public AddProviderLocationDetailsController(ILogger<AddProviderLocationDetailsController> logger)
+        public AddProviderLocationDetailsController(ILogger<AddProviderLocationDetailsController> logger, IMediator mediator)
         {
             _logger = logger;
+            _mediator = mediator;
         }
 
         [Route("{ukprn}/add-training-location/details", Name = RouteNames.GetAddProviderLocationDetails)]
         [HttpGet]
         public IActionResult GetLocationDetails()
         {
-            var (isSuccess, model) = GetViewModel();
-            if (!isSuccess) return RedirectToRouteWithUkprn(RouteNames.GetProviderLocations);
+            var addressItem = GetAddressFromTempData(true);
+            
+            if (addressItem == null) return RedirectToRouteWithUkprn(RouteNames.GetProviderLocations);
+
+            var model = GetViewModel(addressItem);
             return View(ViewPath, model);
         }
 
         [Route("{ukprn}/add-training-location/details", Name = RouteNames.PostAddProviderLocationDetails)]
         [HttpPost]
-        public async Task<IActionResult> SubmitLocationDetails()
+        public async Task<IActionResult> SubmitLocationDetails(ProviderLocationDetailsSubmitModel submitModel)
         {
-            return View();
-        }
+            var addressItem = GetAddressFromTempData(true);
+            if (addressItem == null) return RedirectToRouteWithUkprn(RouteNames.GetProviderLocations);
 
-        private (bool, ProviderLocationDetailsViewModel) GetViewModel()
-        {
-            TempData.TryGetValue(TempDataKeys.SelectedAddressTempDataKey, out var address);
-            TempData.Keep(TempDataKeys.SelectedAddressTempDataKey);
-            if (address == null)
+            if (!ModelState.IsValid)
             {
-                _logger.LogWarning("Selected address not found in the Temp Data, navigating user back to the locations list page");
-                return (false, null);
+                var model = GetViewModel(addressItem);
+
+                model.LocationName = submitModel.LocationName;
+                model.PhoneNumber = submitModel.PhoneNumber;
+                model.Website = submitModel.Website;
+                model.EmailAddress = submitModel.EmailAddress;
+
+                return View(ViewPath, model);
             }
 
-            var addressItem = JsonSerializer.Deserialize<AddressItem>(address.ToString());
+            var command = GetCommand(submitModel, addressItem);
 
+            TempData.Remove(TempDataKeys.SelectedAddressTempDataKey);
+
+            await _mediator.Send(command);
+
+            return RedirectToRouteWithUkprn(RouteNames.GetProviderLocations);
+        }
+
+        private CreateProviderLocationCommand GetCommand(ProviderLocationDetailsSubmitModel submitModel, AddressItem addressItem)
+        {
+            var command = new CreateProviderLocationCommand()
+            {
+                Ukprn = base.Ukprn,
+                UserId = base.UserId,
+                LocationName = submitModel.LocationName,
+                AddressLine1 = addressItem.AddressLine1,
+                AddressLine2 = addressItem.AddressLine2,
+                Town = addressItem.Town,
+                Postcode = addressItem.Postcode,
+                County = addressItem.County,
+                Latitude = addressItem.Latitude,
+                Longitude = addressItem.Longitude,
+                Email = submitModel.EmailAddress,
+                Website = submitModel.Website,
+                Phone = submitModel.PhoneNumber
+            };
+            return command;
+        }
+
+        private ProviderLocationDetailsViewModel GetViewModel(AddressItem addressItem)
+        {
             var model = new ProviderLocationDetailsViewModel(addressItem);
             model.CancelLink = GetUrlWithUkprn(RouteNames.GetProviderLocations);
             model.BackLink = GetUrlWithUkprn(RouteNames.GetProviderLocationAddress);
+            return model;
+        }
 
-            return (true, model);
+        private AddressItem GetAddressFromTempData(bool keepTempData)
+        {
+            TempData.TryGetValue(TempDataKeys.SelectedAddressTempDataKey, out var address);
+            if (address == null)
+            {
+                _logger.LogWarning("Selected address not found in the Temp Data, navigating user back to the locations list page");
+                return null; 
+            }
+            if(keepTempData) TempData.Keep(TempDataKeys.SelectedAddressTempDataKey);
+            return JsonSerializer.Deserialize<AddressItem>(address.ToString());
         }
     }
 }
