152ae17bcc2ee4b9491b24b61f8d5582f3224b4b
Deven Shah <devenshah1974@gmail.com>
2022-07-31 00:48:05 +0100

    CSP-51 Added handler to retrieve available standards for provider

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAvailableProviderStandards/GetAvailableProviderStandardsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetAvailableProviderStandards
+{
+    public class GetAvailableProviderStandardsQueryHandler : IRequestHandler<GetAvailableProviderStandardsQuery, GetAvailableProviderStandardsQueryResult>
+    {
+        private readonly IApiClient _apiClient;
+        private readonly ILogger<GetAvailableProviderStandardsQueryHandler> _logger;
+
+        public GetAvailableProviderStandardsQueryHandler(IApiClient apiClient, ILogger<GetAvailableProviderStandardsQueryHandler> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
+
+        public async Task<GetAvailableProviderStandardsQueryResult> Handle(GetAvailableProviderStandardsQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _apiClient.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses");
+            _logger.LogInformation("Found {count} available courses for ukprn: {ukprn}", result.AvailableCourses.Count, request.Ukprn);
+            return result;
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAvailableProviderStandards/GetAvailableProviderStandardsQueryResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetAvailableProviderStandards
+{
+    public class GetAvailableProviderStandardsQueryResult
+    {
+        public List<StandardLookupModel> AvailableCourses { get; set; } = new List<StandardLookupModel>();
+    }
+}
