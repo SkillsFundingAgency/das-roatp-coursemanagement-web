416688bc8230b473d47eef33618a7b45f2e6d9c0
Deven Shah <devenshah1974@gmail.com>
2022-07-31 01:00:27 +0100

    CSP-51 Added model and validator for select a standard

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Validators/AddAStandard/SelectAStandardSubmitModelValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard;
+using SFA.DAS.Roatp.CourseManagement.Web.Validators.AddAStandard;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Validators.AddAStandard
+{
+    [TestFixture]
+    public class SelectAStandardSubmitModelValidatorTests
+    {
+        [TestCase(0, false)]
+        [TestCase(12, true)]
+        public void SelectedLarsCode_Validation(int larsCode, bool isValid)
+        {
+            var model = new SelectAStandardSubmitModel() { SelectedLarsCode = larsCode };
+            var sut = new SelectAStandardSubmitModelValidator();
+
+            var result = sut.TestValidate(model);
+
+            if (isValid)
+                result.ShouldNotHaveValidationErrorFor(m => m.SelectedLarsCode);
+            else
+                result.ShouldHaveValidationErrorFor(m => m.SelectedLarsCode);
+        }
+    }
+}
