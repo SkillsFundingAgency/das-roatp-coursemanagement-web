68c46e56175ca29c87e674e3be48994736a14d7e
Deven Shah <devenshah1974@gmail.com>
2022-08-03 12:40:29 +0100

    CSP-29 Implements select a standard for add standard journey (#53) (#54)
    * CSP-29 Implements select a standard for add standard journey (#53)
    
    * CSP-29 Enabled link to Add a standard
    
    * CSP-51 Added handler to retrieve available standards for provider
    
    * CSP-51 Added model and validator for select a standard
    
    * CSP-51 Implemented GET endpoint for select a standard
    
    * Ignore hidden mac files
    
    * Additional layout file so we can load local css and js
    
    * Add placeholder text to autocomplete control
    
    Co-authored-by: James King <hello@jamesmichaelking.com>
    
    * CSP-51 Minor fixes to the view
    
    * CSP-51 Fixed UI elements
    
    Co-authored-by: James King <hello@jamesmichaelking.com>

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddAStandard/SelectAStandardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard
+{
+    public class SelectAStandardViewModel : SelectAStandardSubmitModel
+    {
+        public string CancelLink { get; set; }
+        public IEnumerable<SelectListItem> Standards { get; set; }
+    }
+
+    public class SelectAStandardSubmitModel
+    {
+        public int SelectedLarsCode { get; set; }
+
+    }
+}
