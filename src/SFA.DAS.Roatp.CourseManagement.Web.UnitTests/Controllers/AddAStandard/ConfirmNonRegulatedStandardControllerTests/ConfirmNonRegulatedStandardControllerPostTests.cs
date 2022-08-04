e2a5a1d9e7b39383ab1a1ace80c709835b6e41a5
Deven Shah <devenshah1974@gmail.com>
2022-08-04 17:57:45 +0100

    CSP-180 Added controller and view

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/AddAStandard/ConfirmNonRegulatedStandardControllerTests/ConfirmNonRegulatedStandardControllerPostTests.cs
@@ -0,0 +1,102 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Application.Standards.Queries.GetStandardInformation;
+using SFA.DAS.Roatp.CourseManagement.Web.Controllers.AddAStandard;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard;
+using SFA.DAS.Roatp.CourseManagement.Web.Services;
+using SFA.DAS.Roatp.CourseManagement.Web.UnitTests.TestHelpers;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.AddAStandard.ConfirmNonRegulatedStandardControllerTests
+{
+    [TestFixture]
+    public class ConfirmNonRegulatedStandardControllerPostTests
+    {
+        [Test, MoqAutoData]
+        public async Task Post_ModelMissingFromSesson_RedirectsToStandardList(
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Frozen] Mock<ISessionService> sessionServiceMock,
+            [Greedy] ConfirmNonRegulatedStandardController sut,
+            ConfirmNonRegulatedStandardSubmitModel submitModel)
+        {
+            sut.AddDefaultContextWithUser();
+            sessionServiceMock.Setup(s => s.Get<StandardSessionModel>(It.IsAny<string>())).Returns((StandardSessionModel)null);
+
+            var response = await sut.SubmitConfirmationOfStandard(submitModel);
+
+            var result = (RedirectToRouteResult)response;
+            result.Should().NotBeNull();
+            result.RouteName.Should().Be(RouteNames.ViewStandards);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Post_ModelStateIsInvalid_ReturnsViewResult(
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Frozen] Mock<ISessionService> sessionServiceMock,
+            [Greedy] ConfirmNonRegulatedStandardController sut,
+            ConfirmNonRegulatedStandardSubmitModel submitModel,
+            StandardSessionModel sessionModel)
+        {
+            sessionModel.LarsCode = submitModel.LarsCode;
+            sut.AddDefaultContextWithUser();
+            sessionServiceMock.Setup(s => s.Get<StandardSessionModel>(It.IsAny<string>())).Returns(sessionModel);
+            sut.ModelState.AddModelError("key", "message");
+
+            var response = await sut.SubmitConfirmationOfStandard(submitModel);
+
+            var result = (ViewResult)response;
+            result.Should().NotBeNull();
+            result.ViewName.Should().Be(ConfirmNonRegulatedStandardController.ViewPath);
+            mediatorMock.Verify(m => m.Send(It.IsAny<GetStandardInformationQuery>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Test, MoqAutoData]
+        public async Task Post_LarsCodeMismatch_RedirectsToStandardList(
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Frozen] Mock<ISessionService> sessionServiceMock,
+            [Greedy] ConfirmNonRegulatedStandardController sut,
+            ConfirmNonRegulatedStandardSubmitModel submitModel,
+            StandardSessionModel sessionModel)
+        {
+            sessionModel.LarsCode = 1;
+            submitModel.LarsCode = 2;
+            sut.AddDefaultContextWithUser();
+            sessionServiceMock.Setup(s => s.Get<StandardSessionModel>(It.IsAny<string>())).Returns(sessionModel);
+            sut.ModelState.AddModelError("key", "message");
+
+            var response = await sut.SubmitConfirmationOfStandard(submitModel);
+
+            var result = (RedirectToRouteResult)response;
+            result.Should().NotBeNull();
+            result.RouteName.Should().Be(RouteNames.ViewStandards);
+            mediatorMock.Verify(m => m.Send(It.IsAny<GetStandardInformationQuery>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Post_IsNotCorrectStandard_RedirectsToSelectStandard(
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Frozen] Mock<ISessionService> sessionServiceMock,
+            [Greedy] ConfirmNonRegulatedStandardController sut,
+            ConfirmNonRegulatedStandardSubmitModel submitModel,
+            StandardSessionModel sessionModel)
+        {
+            submitModel.LarsCode = sessionModel.LarsCode;
+            submitModel.IsCorrectStandard = false;
+            sut.AddDefaultContextWithUser();
+            sessionServiceMock.Setup(s => s.Get<StandardSessionModel>(It.IsAny<string>())).Returns(sessionModel);
+
+            var response = await sut.SubmitConfirmationOfStandard(submitModel);
+
+            var result = (RedirectToRouteResult)response;
+            result.Should().NotBeNull();
+            result.RouteName.Should().Be(RouteNames.GetAddStandardSelectStandard);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Controllers/AddAStandard/ConfirmNonRegulatedStandardController.cs
@@ -0,0 +1,83 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Authorization.Mvc.Attributes;
+using SFA.DAS.Roatp.CourseManagement.Application.Standards.Queries.GetStandardInformation;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure.Authorization;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard;
+using SFA.DAS.Roatp.CourseManagement.Web.Services;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Controllers.AddAStandard
+{
+    [DasAuthorize(new[] { "ProviderFeature.CourseManagement" }, Policy = nameof(PolicyNames.HasProviderAccount))]
+    public class ConfirmNonRegulatedStandardController : ControllerBase
+    {
+        public const string ViewPath = "~/Views/AddAStandard/ConfirmNonRegulatedStandard.cshtml";
+        private readonly ISessionService _sessionService;
+        private readonly IMediator _mediator;
+        private readonly ILogger<ConfirmNonRegulatedStandardController> _logger;
+
+        public ConfirmNonRegulatedStandardController(ISessionService sessionService, IMediator mediator, ILogger<ConfirmNonRegulatedStandardController> logger)
+        {
+            _sessionService = sessionService;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{ukprn}/standards/add/confirm-standard", Name = RouteNames.GetAddStandardConfirmNonRegulatedStandard)]
+        public async Task<IActionResult> GetConfirmationOfStandard()
+        {
+            var sessionModel = _sessionService.Get<StandardSessionModel>(Ukprn.ToString());
+            if (sessionModel == null || sessionModel.LarsCode == 0)
+            {
+                _logger.LogWarning("Confirm standard: Session model is missing, navigating back.");
+                return RedirectToRouteWithUkprn(RouteNames.ViewStandards);
+            }
+
+            var model = await GetViewModel(sessionModel);
+
+            return View(ViewPath, model);
+        }
+
+        [HttpPost]
+        [Route("{ukprn}/standards/add/confirm-standard", Name = RouteNames.PostAddStandardConfirmNonRegulatedStandard)]
+        public async Task<IActionResult> SubmitConfirmationOfStandard(ConfirmNonRegulatedStandardSubmitModel submitModel)
+        {
+            var sessionModel = _sessionService.Get<StandardSessionModel>(Ukprn.ToString());
+            if (sessionModel == null || sessionModel.LarsCode == 0 || sessionModel.LarsCode != submitModel.LarsCode)
+            {
+                _logger.LogWarning("Confirm standard: Session model is missing, navigating back.");
+                return RedirectToRouteWithUkprn(RouteNames.ViewStandards);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var model = await GetViewModel(sessionModel);
+
+                return View(ViewPath, model);
+            }
+
+            if (submitModel.IsCorrectStandard == false)
+            {
+                return RedirectToRouteWithUkprn(RouteNames.GetAddStandardSelectStandard);
+            }
+
+            return Ok();
+        }
+
+        private async Task<ConfirmNonRegulatedStandardViewModel> GetViewModel(StandardSessionModel sessionModel)
+        {
+            var standardInfo = await _mediator.Send(new GetStandardInformationQuery(sessionModel.LarsCode));
+            var model = new ConfirmNonRegulatedStandardViewModel()
+            {
+                StandardInformation = standardInfo,
+                LarsCode = standardInfo.LarsCode,
+                CancelLink = GetUrlWithUkprn(RouteNames.ViewStandards)
+            };
+            return model;
+        }
+    }
+}
