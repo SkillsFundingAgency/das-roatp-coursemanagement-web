7a47435ecaa45f39b5ee8340d5ae981d9f5f3fc1
Deven Shah <devenshah1974@gmail.com>
2022-08-04 16:35:04 +0100

    CSP-180 Implemented validator for standard confirmation

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Validators/AddAStandard/ConfirmStandardSubmitModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.Validators.AddAStandard
+{
+    public class ConfirmStandardSubmitModelValidator : AbstractValidator<ConfirmNonRegulatedStandardSubmitModel>
+    {
+        public const string ConfirmationAnswerMissingMessage = "Answer the question by selecting either yes or no";
+        public ConfirmStandardSubmitModelValidator()
+        {
+            RuleFor(c => c.IsCorrectStandard)
+                .NotEmpty()
+                .WithMessage(ConfirmationAnswerMissingMessage);
+        }
+    }
+}
