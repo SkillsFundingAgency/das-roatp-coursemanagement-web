b6a5ee1076dd5640ef36b25a363cc9d7d01eb1ef
Deven Shah <devenshah1974@gmail.com>
2022-08-04 10:50:13 +0100

    CSP-180 Added handler to retrieve standard information

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/Standards/Queries/GetStandardInformation/GetStandardInformationQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.Standards.Queries.GetStandardInformation
+{
+    public class GetStandardInformationQueryHandler : IRequestHandler<GetStandardInformationQuery, GetStandardInformationQueryResult>
+    {
+        private readonly IApiClient _apiClient;
+        private readonly ILogger<GetStandardInformationQueryHandler> _logger;
+
+        public GetStandardInformationQueryHandler(IApiClient apiClient, ILogger<GetStandardInformationQueryHandler> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
+
+        public async Task<GetStandardInformationQueryResult> Handle(GetStandardInformationQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _apiClient.Get<GetStandardInformationQueryResult>($"lookup/standards/{request.LarsCode}");
+
+            if (result == null)
+            {
+                throw new InvalidOperationException($"Standard information not found for larscode: {request.LarsCode}");
+            }
+
+            return result;
+        }
+    }
+}
