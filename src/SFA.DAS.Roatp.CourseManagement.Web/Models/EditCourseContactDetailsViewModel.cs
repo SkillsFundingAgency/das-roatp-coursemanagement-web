003836c78c89fa17a98efc9758a19cd1354b3819
Deven Shah <devenshah1974@gmail.com>
2022-08-08 14:58:22 +0100

    CSP-132 Minor refactoring

--- src/SFA.DAS.Roatp.CourseManagement.Web/Models/EditCourseContactDetailsViewModel.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/EditCourseContactDetailsViewModel.cs
@@ -1,9 +1,12 @@
-﻿using SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetStandardDetails;
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetStandardDetails;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Models
 {
     public class EditCourseContactDetailsViewModel : EditCourseContactDetailsSubmitModel
     {
+        [FromRoute]
+        public int LarsCode { get; set; }
         public string BackLink { get; set; }
         public string CancelLink { get; set; }
         public int ProviderCourseId { get; set; }
