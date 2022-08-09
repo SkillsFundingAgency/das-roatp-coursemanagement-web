db67e0d65ade2e4a574013eb9ea810ac286783c1
Deven Shah <devenshah1974@gmail.com>
2022-08-09 08:30:32 +0100

    CSP-133 Implements select location option for add standard journey

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/AddAStandard/SelectLocationOptionControllerTests/SelectLocationOptionControllerGetTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SFA.DAS.Testing.AutoFixture;
+using SFA.DAS.Roatp.CourseManagement.Web.Controllers.AddAStandard;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard;
+using SFA.DAS.Roatp.CourseManagement.Web.UnitTests.TestHelpers;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.AddAStandard.SelectLocationOptionControllerTests
+{
+    [TestFixture]
+    public class SelectLocationOptionControllerGetTests
+    {
+        [Test, MoqAutoData]
+        public void SelectLocationOption_ReturnsView(
+            [Greedy] SelectLocationOptionController sut,
+            string cancelLink)
+        {
+            sut.AddDefaultContextWithUser().AddUrlHelperMock().AddUrlForRoute(RouteNames.ViewStandards, cancelLink);
+
+            var result = sut.SelectLocationOption();
+
+            result.As<ViewResult>().Should().NotBeNull();
+            result.As<ViewResult>().ViewName.Should().Be(SelectLocationOptionController.ViewPath);
+            result.As<ViewResult>().Model.As<SelectLocationOptionViewModel>().Should().NotBeNull();
+            result.As<ViewResult>().Model.As<SelectLocationOptionViewModel>().CancelLink.Should().Be(cancelLink);
+        }
+    }
+}
