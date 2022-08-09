ecb3a70fc8d7d4603e3d34f830aaa33718babb46
Deven Shah <devenshah1974@gmail.com>
2022-08-09 15:10:28 +0100

    CSP-213 Implements national delivery question

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddAStandard/ConfirmNationalProviderViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard
+{
+    public class ConfirmNationalProviderViewModel : ConfirmNationalProviderSubmitModel
+    {
+        public string CancelLink { get; set; }
+    }
+}
