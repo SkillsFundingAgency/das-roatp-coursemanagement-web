fc0240f5029bb8e4137b744dae0631c1043a3d7c
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-12 15:09:24 +0100

    CSP-184 validator unit tests

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Validators/ProviderCourseLocationAddSubmitModelValidatorTests/TrainingVenueNavigationIdValidationTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.ProviderCourseLocations;
+using SFA.DAS.Roatp.CourseManagement.Web.Validators;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Validators.ProviderCourseLocationAddSubmitModelValidatorTests
+{
+    [TestFixture]
+    public class TrainingVenueNavigationIdValidationTests
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("     ")]
+        public void TrainingVenueNavigationId_NullOrEmpty_IsInValid(string trainingVenueNavigation)
+        {
+            var sut = new ProviderCourseLocationAddSubmitModelValidator();
+
+            var result = sut.TestValidate(new ProviderCourseLocationAddSubmitModel() { TrainingVenueNavigationId = trainingVenueNavigation });
+
+            result.IsValid.Should().BeFalse();
+            result.ShouldHaveValidationErrorFor(m => m.TrainingVenueNavigationId).WithErrorMessage(ProviderCourseLocationAddSubmitModelValidator.TrainingVenueErrorMessage);
+        }
+    }
+}
