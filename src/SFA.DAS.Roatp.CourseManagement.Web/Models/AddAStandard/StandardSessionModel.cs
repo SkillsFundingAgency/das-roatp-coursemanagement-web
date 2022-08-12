b6bb7f5ff14c7eb1aae7e8cfd776ba599e34057f
Deven Shah <devenshah1974@gmail.com>
2022-08-12 15:21:29 +0100

    CSP-214 Implments standard review page

--- src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddAStandard/StandardSessionModel.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/AddAStandard/StandardSessionModel.cs
@@ -1,5 +1,9 @@
-﻿using SFA.DAS.Roatp.CourseManagement.Domain.Models;
+﻿using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using SFA.DAS.Roatp.CourseManagement.Domain.Models;
 using SFA.DAS.Roatp.CourseManagement.Web.Models.Standards;
+using SFA.DAS.Roatp.CourseManagement.Web.Services;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard
 {
@@ -11,5 +15,10 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Models.AddAStandard
         public StandardContactInformationViewModel ContactInformation { get; set; } = new StandardContactInformationViewModel();
         public LocationOption LocationOption { get; set; }
         public bool? HasNationalDeliveryOption { get; set; }
+        public List<CourseLocationModel> CourseLocations { get; set; } = new List<CourseLocationModel>();
+        public IEnumerable<CourseLocationModel> ProviderLocations => CourseLocations.Where(l => l.LocationType == LocationType.Provider);
+        public IEnumerable<IGrouping<string, CourseLocationModel>> RegionalLocations => CourseLocations.Where(l => l.LocationType == LocationType.Regional).GroupBy(l => l.RegionName).OrderBy(g => g.Key);
+        public string LocationSummary => LocationSummaryCalculator.GetLocationSummary(HasNationalDeliveryOption.GetValueOrDefault(), ProviderLocations.Any(), RegionalLocations.Any());
+        public string CancelLink { get; set; }
     }
 }
