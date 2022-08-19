75e6b046c249ba0c57d3893b4a3facae6b05b0d5
Deven Shah <devenshah1974@gmail.com>
2022-08-19 09:04:35 +0100

    CSP-215 Added handler for create provider course

---
+++ src/SFA.DAS.Roatp.CourseManagement.Application.UnitTests/ProviderStandards/Commands/AddProviderCourse/AddProviderCourseCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Commands.CreateProviderLocation;
+using SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Commands.AddProviderCourse;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.UnitTests.ProviderStandards.Commands.AddProviderCourse
+{
+    [TestFixture]
+    public class AddProviderCourseCommandHandlerTests
+    {
+        [Test, MoqAutoData]
+        public async Task Handle_InvokesApiClient(
+            [Frozen] Mock<IApiClient> apiClientMock,
+            AddProviderCourseCommandHandler sut,
+            AddProviderCourseCommand command)
+        {
+            apiClientMock.Setup(a => a.Post($"providers/{command.Ukprn}/courses/{command.LarsCode}/create", command)).ReturnsAsync(HttpStatusCode.Created);
+
+            await sut.Handle(command, new CancellationToken());
+
+            apiClientMock.Verify(a => a.Post($"providers/{command.Ukprn}/courses/{command.LarsCode}/create", command));
+        }
+
+        [Test, MoqAutoData]
+        public async Task Handle_ApiClientDoesNotReturnCreatedStatus_ThrowsException(
+            [Frozen] Mock<IApiClient> apiClientMock,
+            AddProviderCourseCommandHandler sut,
+            AddProviderCourseCommand command)
+        {
+            apiClientMock.Setup(a => a.Post($"providers/{command.Ukprn}/courses/{command.LarsCode}/create", command)).ReturnsAsync(HttpStatusCode.BadRequest);
+
+            Func<Task> action = () => sut.Handle(command, new CancellationToken());
+
+            await action.Should().ThrowAsync<InvalidOperationException>();
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Commands/AddProviderCourse/AddProviderCourseCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Commands.AddProviderCourse
+{
+    public class AddProviderCourseCommandHandler : IRequestHandler<AddProviderCourseCommand, Unit>
+    {
+        private readonly IApiClient _apiClient;
+        private readonly ILogger<AddProviderCourseCommandHandler> _logger;
+
+        public AddProviderCourseCommandHandler(ILogger<AddProviderCourseCommandHandler> logger, IApiClient apiClient)
+        {
+            _logger = logger;
+            _apiClient = apiClient;
+        }
+
+        public async Task<Unit> Handle(AddProviderCourseCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Command triggered to create course: {larscode} for ukprn: {ukprn} by user:{userid}", request.LarsCode, request.Ukprn, request.UserId);
+
+            var statusCode = await _apiClient.Post($"providers/{request.Ukprn}/courses/{request.LarsCode}/create", request);
+
+            if (statusCode != HttpStatusCode.Created)
+            {
+                _logger.LogError("Failed to create provider course: {larscode} for ukprn:{ukprn} by user:{userid}", request.LarsCode, request.Ukprn, request.UserId);
+                var exception = new System.InvalidOperationException($"Response to create provider course {request.LarsCode} for ukprn:{request.Ukprn} did not come back with success code");
+                throw exception;
+            }
+
+            return Unit.Value;
+        }
+    }
+}
