e1ae70a895fbd56d3ee306a47b7ae488d3006c6c
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2022-11-28 14:50:00 +0000

    CSP-422-Audit entries during updating provider location (#95)
    CSP-422-Audit entries during updating provider location (#95)

--- src/SFA.DAS.Roatp.CourseManagement.Application/ProviderLocations/Commands/UpdateProviderLocationDetails/UpdateProviderLocationDetailsCommand.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderLocations/Commands/UpdateProviderLocationDetails/UpdateProviderLocationDetailsCommand.cs
@@ -8,6 +8,7 @@ namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Commands.
         public int Ukprn { get; set; }
         public Guid Id { get; set; }
         public string UserId { get; set; }
+        public string UserDisplayName { get; set; }
         public string LocationName { get; set; }
         public string Website { get; set; }
         public string Email { get; set; }
