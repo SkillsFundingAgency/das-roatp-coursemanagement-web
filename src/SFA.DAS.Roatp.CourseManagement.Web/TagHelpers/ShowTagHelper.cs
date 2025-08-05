ce144fa54bf6a19902134df383f160c99a7b072e
Deven Shah <devenshah1974@gmail.com>
2025-08-05 19:42:46 +0100

    CSP-2084 Added missing tag helper

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web/TagHelpers/ShowTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.TagHelpers;
+
+[HtmlTargetElement(Attributes = "asp-show")]
+public class ShowTagHelper : TagHelper
+{
+    public bool? AspShow { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (!AspShow.HasValue || !AspShow.Value)
+        {
+            output.SuppressOutput();
+        }
+    }
+}
