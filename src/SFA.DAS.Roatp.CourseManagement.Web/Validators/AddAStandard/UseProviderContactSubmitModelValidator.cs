436c2510cc67d2d81331905fb8b6e8ccdee20c07
MarkFCain <mark.cain@digital.education.gov.uk>
2025-09-12 13:54:50 +0100

    CSP-2145 validator coverage added

--- src/SFA.DAS.Roatp.CourseManagement.Web/Validators/AddAStandard/UseProviderContactSubmitModelValidator.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Validators/AddAStandard/UseProviderContactSubmitModelValidator.cs
@@ -6,11 +6,11 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.Validators.AddAStandard;
 public class UseSavedContactDetailsViewModelValidator
     : AbstractValidator<UseSavedContactDetailsViewModel>
 {
-    public const string ConfirmationAnswerMissingMessage = "Select yes if you want to use your saved contact details";
+    public const string UseSavedContactDetailsAnswerMissingMessage = "Select yes if you want to use your saved contact details";
     public UseSavedContactDetailsViewModelValidator()
     {
         RuleFor(c => c.IsUsingSavedContactDetails)
             .NotEmpty()
-            .WithMessage(ConfirmationAnswerMissingMessage);
+            .WithMessage(UseSavedContactDetailsAnswerMissingMessage);
     }
 }
