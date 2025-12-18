9d86f0f2a0e73508a853214a38a6d4abd9c81a66
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2025-12-18 14:25:24 +0000

    CSP-2335 improved coverage for validator

--- src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Validators/AddAStandard/SelectAStandardSubmitModelValidatorTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Validators/AddAStandard/SelectAStandardSubmitModelValidatorTests.cs
@@ -8,8 +8,15 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Validators.AddAStandard
     [TestFixture]
     public class SelectAStandardSubmitModelValidatorTests
     {
+        [TestCase(null, false)]
+        [TestCase("", false)]
         [TestCase("0", false)]
+        [TestCase("-1", false)]
+        [TestCase("abc", false)]
+        [TestCase("12.5", false)]
+        [TestCase("1", true)]
         [TestCase("12", true)]
+        [TestCase("123456", true)]
         public void SelectedLarsCode_Validation(string larsCode, bool isValid)
         {
             var model = new SelectAStandardSubmitModel() { SelectedLarsCode = larsCode };
@@ -21,6 +28,30 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Validators.AddAStandard
                 result.ShouldNotHaveValidationErrorFor(m => m.SelectedLarsCode);
             else
                 result.ShouldHaveValidationErrorFor(m => m.SelectedLarsCode);
+        }
+
+        [Test]
+        public void SelectedLarsCode_Empty_ReturnsCorrectErrorMessage()
+        {
+            var model = new SelectAStandardSubmitModel() { SelectedLarsCode = string.Empty };
+            var sut = new SelectAStandardSubmitModelValidator();
+
+            var result = sut.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(m => m.SelectedLarsCode)
+                .WithErrorMessage(SelectAStandardSubmitModelValidator.StandardIsRequiredMesssage);
+        }
+
+        [Test]
+        public void SelectedLarsCode_Invalid_ReturnsCorrectErrorMessage()
+        {
+            var model = new SelectAStandardSubmitModel() { SelectedLarsCode = "0" };
+            var sut = new SelectAStandardSubmitModelValidator();
+
+            var result = sut.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(m => m.SelectedLarsCode)
+                .WithErrorMessage(SelectAStandardSubmitModelValidator.StandardIsRequiredMesssage);
         }
     }
 }
