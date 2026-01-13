b5d71ec3b48eb467d24ee4074f6802da9e399989
afzal-hussain259 <afi259@hotmail.co.uk>
2026-01-13 17:17:53 +0000

    CSP-2367 Include course type to available courses (#161)
    * CSP-2367 Include course type to available courses
    
    * CSP-2367 added test coverage

--- src/SFA.DAS.Roatp.CourseManagement.Application.UnitTests/ProviderStandards/Queries/GetAvailableProviderStandards/GetAvailableProviderStandardsQueryHandlerTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Application.UnitTests/ProviderStandards/Queries/GetAvailableProviderStandards/GetAvailableProviderStandardsQueryHandlerTests.cs
@@ -8,24 +8,23 @@ using SFA.DAS.Testing.AutoFixture;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.Roatp.CourseManagement.Application.UnitTests.ProviderStandards.Queries.GetAvailableProviderStandards
-{
-    [TestFixture]
-    public class GetAvailableProviderStandardsQueryHandlerTests
-    {
-        [Test, MoqAutoData]
-        public async Task Handler_CallsApiClient_ReturnsResult(
-            [Frozen] Mock<IApiClient> apiClientMock,
-            GetAvailableProviderStandardsQueryResult expectedResult,
-            GetAvailableProviderStandardsQuery request,
-            GetAvailableProviderStandardsQueryHandler sut)
-        {
-            apiClientMock.Setup(a => a.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses")).ReturnsAsync(expectedResult);
-            
-            var actualResult = await sut.Handle(request, new CancellationToken());
+namespace SFA.DAS.Roatp.CourseManagement.Application.UnitTests.ProviderStandards.Queries.GetAvailableProviderStandards;
 
-            apiClientMock.Verify(x => x.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses"), Times.Once);
-            expectedResult.Should().BeEquivalentTo(actualResult);
-        }
+[TestFixture]
+public class GetAvailableProviderStandardsQueryHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handler_CallsApiClient_ReturnsResult(
+        [Frozen] Mock<IApiClient> apiClientMock,
+        GetAvailableProviderStandardsQueryResult expectedResult,
+        GetAvailableProviderStandardsQuery request,
+        GetAvailableProviderStandardsQueryHandler sut)
+    {
+        apiClientMock.Setup(a => a.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses/{request.CourseType}")).ReturnsAsync(expectedResult);
+
+        var actualResult = await sut.Handle(request, new CancellationToken());
+
+        apiClientMock.Verify(x => x.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses/{request.CourseType}"), Times.Once);
+        expectedResult.Should().BeEquivalentTo(actualResult);
     }
 }
--- src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAvailableProviderStandards/GetAvailableProviderStandardsQueryHandler.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAvailableProviderStandards/GetAvailableProviderStandardsQueryHandler.cs
@@ -4,24 +4,23 @@ using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetAvailableProviderStandards
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetAvailableProviderStandards;
+
+public class GetAvailableProviderStandardsQueryHandler : IRequestHandler<GetAvailableProviderStandardsQuery, GetAvailableProviderStandardsQueryResult>
 {
-    public class GetAvailableProviderStandardsQueryHandler : IRequestHandler<GetAvailableProviderStandardsQuery, GetAvailableProviderStandardsQueryResult>
+    private readonly IApiClient _apiClient;
+    private readonly ILogger<GetAvailableProviderStandardsQueryHandler> _logger;
+
+    public GetAvailableProviderStandardsQueryHandler(IApiClient apiClient, ILogger<GetAvailableProviderStandardsQueryHandler> logger)
     {
-        private readonly IApiClient _apiClient;
-        private readonly ILogger<GetAvailableProviderStandardsQueryHandler> _logger;
+        _apiClient = apiClient;
+        _logger = logger;
+    }
 
-        public GetAvailableProviderStandardsQueryHandler(IApiClient apiClient, ILogger<GetAvailableProviderStandardsQueryHandler> logger)
-        {
-            _apiClient = apiClient;
-            _logger = logger;
-        }
-
-        public async Task<GetAvailableProviderStandardsQueryResult> Handle(GetAvailableProviderStandardsQuery request, CancellationToken cancellationToken)
-        {
-            var result = await _apiClient.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses");
-            _logger.LogInformation("Found {count} available courses for ukprn: {ukprn}", result.AvailableCourses.Count, request.Ukprn);
-            return result;
-        }
+    public async Task<GetAvailableProviderStandardsQueryResult> Handle(GetAvailableProviderStandardsQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _apiClient.Get<GetAvailableProviderStandardsQueryResult>($"providers/{request.Ukprn}/available-courses/{request.CourseType}");
+        _logger.LogInformation("Found {count} available courses for ukprn: {ukprn}", result.AvailableCourses.Count, request.Ukprn);
+        return result;
     }
 }
