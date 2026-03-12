7091685eecbfa4525bb772a0e3e9c42903656dc7
Afzal Hussain <afi259@hotmail.co.uk>
2026-03-12 13:42:18 +0000

    CSP-2524 Refactured code to use a single shared view.

--- src/SFA.DAS.Roatp.CourseManagement.Web/Models/ProviderContact/ConfirmUpdateStandardsViewModel.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web/Models/ProviderContact/ConfirmUpdateStandardsViewModel.cs
@@ -9,4 +9,7 @@ public class ConfirmUpdateStandardsSubmitViewModel
     public bool? HasOptedToUpdateExistingStandards { get; set; }
     public string EmailAddress { get; set; }
     public string PhoneNumber { get; set; }
+    public bool EmailAddressOnlyUpdate { get; set; }
+    public bool PhoneNumberOnlyUpdate { get; set; }
+    public bool EmailAddressAndPhoneNumberUpdate { get; set; }
 }
