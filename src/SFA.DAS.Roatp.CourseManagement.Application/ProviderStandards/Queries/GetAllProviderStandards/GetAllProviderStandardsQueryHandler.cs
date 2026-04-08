8d961c3d9c1d2c21a19ef2b9acb99b50229e128d
Deven Shah <devenshah1974@gmail.com>
2026-04-08 14:49:01 +0100

    CSP-2539 Fixes error on review page (#206)

--- src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAllProviderStandards/GetAllProviderStandardsQueryHandler.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Application/ProviderStandards/Queries/GetAllProviderStandards/GetAllProviderStandardsQueryHandler.cs
@@ -1,45 +1,35 @@
-﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Domain.Interfaces;
 
 
-namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetAllProviderStandards
+namespace SFA.DAS.Roatp.CourseManagement.Application.ProviderStandards.Queries.GetAllProviderStandards;
+
+public class GetAllProviderStandardsQueryHandler : IRequestHandler<GetAllProviderStandardsQuery, GetAllProviderStandardsQueryResult>
 {
-    public class GetAllProviderStandardsQueryHandler : IRequestHandler<GetAllProviderStandardsQuery, GetAllProviderStandardsQueryResult>
+    private readonly ILogger<GetAllProviderStandardsQueryHandler> _logger;
+    private readonly IApiClient _apiClient;
+    public GetAllProviderStandardsQueryHandler(IApiClient apiClient, ILogger<GetAllProviderStandardsQueryHandler> logger)
     {
-        private readonly ILogger<GetAllProviderStandardsQueryHandler> _logger;
-        private readonly IApiClient _apiClient;
-        public GetAllProviderStandardsQueryHandler(IApiClient apiClient, ILogger<GetAllProviderStandardsQueryHandler> logger)
+        _logger = logger;
+        _apiClient = apiClient;
+    }
+    public async Task<GetAllProviderStandardsQueryResult> Handle(GetAllProviderStandardsQuery request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Get Standards request received for Ukprn number {Ukprn}", request.Ukprn);
+        var standards = await _apiClient.Get<List<Domain.ApiModels.Standard>>($"providers/{request.Ukprn}/courses?courseType={request.CourseType}");
+        if (standards == null)
         {
-            _logger = logger;
-            _apiClient = apiClient;
+            _logger.LogInformation("Courses data not found for {Ukprn}", request.Ukprn);
+            return new GetAllProviderStandardsQueryResult();
         }
-        public async Task<GetAllProviderStandardsQueryResult> Handle(GetAllProviderStandardsQuery request, CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Get Standards request received for Ukprn number {ukprn}", request.Ukprn);
-            try
-            {
-                var standards = await _apiClient.Get<List<Domain.ApiModels.Standard>>($"providers/{request.Ukprn}/courses?courseType={request.CourseType}");
-                if (standards == null)
-                {
-                    _logger.LogInformation("Courses data not found for {ukprn}", request.Ukprn);
-                    return null;
-                }
 
-                return new GetAllProviderStandardsQueryResult
-                {
-                    Standards = standards
-                };
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"Error occurred trying to retrieve Standards for Ukprn {request.Ukprn}");
-                throw;
-            }
-        }
+        return new GetAllProviderStandardsQueryResult
+        {
+            Standards = standards
+        };
     }
 }
