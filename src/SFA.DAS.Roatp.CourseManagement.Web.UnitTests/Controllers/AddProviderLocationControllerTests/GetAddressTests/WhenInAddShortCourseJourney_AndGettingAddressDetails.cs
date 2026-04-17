2737b5481c3083971557120fb6bee2ec61d55e31
Afzal Hussain <afi259@hotmail.co.uk>
2026-04-17 01:55:35 +0100

    CSP-2556 Refactored add provider location UTs

---
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/AddProviderLocationControllerTests/GetAddressTests/WhenInAddShortCourseJourney_AndGettingAddressDetails.cs
@@ -0,0 +1,229 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
+using SFA.DAS.Roatp.CourseManagement.Web.Common.Constants;
+using SFA.DAS.Roatp.CourseManagement.Web.Controllers;
+using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
+using SFA.DAS.Roatp.CourseManagement.Web.Models;
+using SFA.DAS.Roatp.CourseManagement.Web.Models.ShortCourses.AddAShortCourse;
+using SFA.DAS.Roatp.CourseManagement.Web.Services;
+using SFA.DAS.Roatp.CourseManagement.Web.UnitTests.TestHelpers;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.AddProviderLocationControllerTests.GetAddressTests;
+public class WhenInAddShortCourseJourney_AndGettingAddressDetails
+{
+    private ApprenticeshipType _learningType;
+
+    [SetUp]
+    public void Before_Each_Test()
+    {
+        _learningType = ApprenticeshipType.ApprenticeshipUnit;
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_SessionIsValid_Then_ReturnsExpectedViewModel(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as ViewResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.ViewName.Should().Be(AddProviderLocationController.ViewPath);
+        result.Model.Should().BeOfType<AddProviderLocationViewModel>();
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_SessionIsValid_Then_RouteIsPostAddProviderLocation(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as ViewResult;
+
+        // Assert
+        var model = result.Model as AddProviderLocationViewModel;
+        model.Route.Should().Be(RouteNames.PostAddProviderLocation);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_SessionIsValid_Then_IsAddJourneyIsTrue(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as ViewResult;
+
+        // Assert
+        var model = result.Model as AddProviderLocationViewModel;
+        model.IsAddJourney.Should().BeTrue();
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_SessionIsValid_Then_CorrectDisplayHeaderIsReturned(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as ViewResult;
+
+        // Assert
+        var model = result.Model as AddProviderLocationViewModel;
+        model.DisplayHeader.Should().Be("Add an apprenticeship unit");
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_HasSeenSummaryPageIsTrue_Then_SubmitButtonTextIsConfirm(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sessionModel.HasSeenSummaryPage = true;
+
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as ViewResult;
+
+        // Assert
+        var model = result.Model as AddProviderLocationViewModel;
+        model.SubmitButtonText.Should().Be(ButtonText.Confirm);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_HasSeenSummaryPageIsFalse_Then_SubmitButtonTextIsContinue(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sessionModel.HasSeenSummaryPage = false;
+
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as ViewResult;
+
+        // Assert
+        var model = result.Model as AddProviderLocationViewModel;
+        model.SubmitButtonText.Should().Be(ButtonText.Continue);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_SessionIsNull_Then_RedirectsToReviewYourDetails(
+        Mock<ITempDataDictionary> tempDataMock,
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Greedy] AddProviderLocationController sut)
+    {
+        // Arrange
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns((ShortCourseSessionModel)null);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as RedirectToRouteResult;
+
+        // Assert
+        result!.RouteName.Should().Be(RouteNames.ReviewYourDetails);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_LocationsAvailableIsTrueInSession_Then_RedirectsToSelectShortCourseTrainingVenue(
+        Mock<ITempDataDictionary> tempDataMock,
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sessionModel.LocationsAvailable = true;
+
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        var result = await sut.GetAddress(_learningType) as RedirectToRouteResult;
+
+        // Assert
+        result!.RouteName.Should().Be(RouteNames.SelectShortCourseTrainingVenue);
+    }
+
+    [Test, MoqAutoData]
+    public async Task When_SessionIsValid_Then_VerifyTempDataIsRemoved(
+        [Frozen] Mock<ISessionService> sessionServiceMock,
+        [Frozen] Mock<ITempDataDictionary> tempDataMock,
+        [Greedy] AddProviderLocationController sut,
+        ShortCourseSessionModel sessionModel)
+    {
+        // Arrange
+        sut.AddDefaultContextWithUser();
+
+        sut.TempData = tempDataMock.Object;
+
+        sessionServiceMock.Setup(s => s.Get<ShortCourseSessionModel>()).Returns(sessionModel);
+
+        // Act
+        await sut.GetAddress(_learningType);
+
+        // Assert
+        tempDataMock.Verify(t => t.Remove(TempDataKeys.SelectedTrainingVenueAddressTempDataKey));
+    }
+}
