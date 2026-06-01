c7e2daa628489223800f5e4ab8f18222aefeab8a
Afzal Hussain <afi259@hotmail.co.uk>
2026-06-01 16:37:06 +0100

    CSP-2603 Added test coverage

--- src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/ViewProviderLocationDetailsControllerTests/ViewProviderLocationDetailsControllerTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Web.UnitTests/Controllers/ViewProviderLocationDetailsControllerTests/ViewProviderLocationDetailsControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit4;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Roatp.CourseManagement.Application.ProviderLocations.Queries.GetProviderLocationDetails;
+using SFA.DAS.Roatp.CourseManagement.Domain.ApiModels;
 using SFA.DAS.Roatp.CourseManagement.Web.Controllers;
 using SFA.DAS.Roatp.CourseManagement.Web.Infrastructure;
 using SFA.DAS.Roatp.CourseManagement.Web.Models.ProviderLocations;
@@ -25,6 +28,8 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.ViewProviderL
         private readonly string verifyUrl = "http://test";
         private readonly string verifyUpdateProviderLocationDetailsUrl = "http://test-UpdateProviderLocationDetailsUrl";
         private readonly string verifySelectCourseTypeUrl = "http://test-SelectCourseType";
+        private readonly string standardLinkUrl = "http://test-StandardLink";
+        private readonly string apprenticeshipUnitUrl = "http://test-ApprenticeshipUnitLink";
 
         [SetUp]
         public void Before_Each_Test()
@@ -35,7 +40,9 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.ViewProviderL
                 .AddUrlHelperMock()
                 .AddUrlForRoute(RouteNames.GetProviderLocations, verifyUrl)
                 .AddUrlForRoute(RouteNames.GetUpdateProviderLocationDetails, verifyUpdateProviderLocationDetailsUrl)
-                .AddUrlForRoute(RouteNames.SelectCourseType, verifySelectCourseTypeUrl);
+                .AddUrlForRoute(RouteNames.SelectCourseType, verifySelectCourseTypeUrl)
+                .AddUrlForRoute(RouteNames.GetStandardDetails, standardLinkUrl)
+                .AddUrlForRoute(RouteNames.ManageShortCourseDetails, apprenticeshipUnitUrl);
         }
 
         [Test, AutoData]
@@ -70,6 +77,43 @@ namespace SFA.DAS.Roatp.CourseManagement.Web.UnitTests.Controllers.ViewProviderL
             var viewResult = result as ViewResult;
             viewResult.Should().NotBeNull();
             viewResult!.ViewName.Should().Be(ViewsPath.PageNotFoundPath);
+        }
+
+        [Test, AutoData]
+        public async Task GetProviderLocationDetails_PopulateCourseLinks(
+            GetProviderLocationDetailsQueryResult queryResult,
+            Guid id)
+        {
+            queryResult.ProviderLocation.Standards = new List<LocationStandardModel>()
+            {
+                new LocationStandardModel()
+                {
+                    Title = "Test Standard",
+                    Level = 2,
+                    LarsCode = "12345678",
+                    LearningType = ApprenticeshipType.Apprenticeship
+                },
+                new LocationStandardModel()
+                {
+                    Title = "Test Apprenticeship Unit",
+                    Level = 2,
+                    LarsCode = "98765432",
+                    LearningType = ApprenticeshipType.ApprenticeshipUnit
+                }
+            };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.Is<GetProviderLocationDetailsQuery>(q => q.Ukprn == int.Parse(Ukprn) && q.Id == id), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(queryResult);
+
+            var result = await _sut.GetProviderLocationDetails(id);
+
+            var viewResult = result as ViewResult;
+            var model = viewResult.Model as ProviderLocationViewModel;
+            model.StandardLinks.Courses.First().CourseName.Should().Be("Test Standard (level 2)");
+            model.StandardLinks.Courses.First().Url.Should().Be(standardLinkUrl);
+            model.ApprenticeshipUnitLinks.Courses.First().CourseName.Should().Be("Test Apprenticeship Unit (level 2)");
+            model.ApprenticeshipUnitLinks.Courses.First().Url.Should().Be(apprenticeshipUnitUrl);
         }
     }
 }
